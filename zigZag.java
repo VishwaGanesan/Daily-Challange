8b2a5878f076dcc430319cbfd2f2afa47292edb8
Vishwa <gvishwa1997@gmail.com>
2018-07-18 22:45:06 +0530

    Update zigZag.java

--- zigZag.java
+++ zigZag.java
@@ -1,7 +1,6 @@
 import java.util.*;
 class Hello {
-
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 		//Your Code Here
 		Scanner s = new Scanner(System.in);
 		int n = s.nextInt();
@@ -10,14 +9,14 @@ class Hello {
 		int adder = -1,x = 0,flag = 0;
 		char[][] matrix = new char[n][n];
 		
-    i_center = n%2==0?(n/2):(n/2)+1;
+                i_center = n%2==0?(n/2):(n/2)+1;
 		j_center = n;
 		
 		for(int i = 0;i < n;i += 1)
 		{
 		    for(int j = 0;j < n;j += 1)
 		    {
-		        matrix[i][j] = s.next().charAt(0);
+		        matrix[i][j] = s.next().charAt(0);    // get an char Array
 		    }
 		}
 		
@@ -29,7 +28,7 @@ class Hello {
 		    }
 		    for(int j = 0;j < j_center;j += 1)
 		    {
-		        if(matrix[i][ (n-1) - rj] == matrix[ri][rj])
+		        if(matrix[i][(n-1) - rj] == matrix[ri][rj])
 		        {
 		            System.out.print(matrix[ri][rj]+" ");
 		            flag = 1;
